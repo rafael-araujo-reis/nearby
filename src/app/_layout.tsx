2e6c1e9b237c7eaef57555cfc580a6a7ae0296e4
Rafael Reis <rafa.araujoreis@gmail.com>
2024-12-12 22:44:48 -0300

    add animation bottom sheet

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Loading } from "@/components/loading";
 import { colors } from "@/styles/theme";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import {
   Rubik_400Regular,
   Rubik_500Medium,
@@ -22,11 +23,13 @@ export default function Layout() {
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: colors.gray[100] },
-      }}
-    />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: colors.gray[100] },
+        }}
+      />
+    </GestureHandlerRootView>
   );
 }
